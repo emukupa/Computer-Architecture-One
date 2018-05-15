eab39075831afb1ce5a07b2c099bc28fd8cc3f3f
Edward Manda <lilkups@yahoo.com>
2018-05-15 18:00:47 -0500

    i

--- README.md
+++ README.md
@@ -17,11 +17,11 @@
 # LS-8 Emulator Multiply
 
 * [x] Step 1: Implement missing features in ram.js
-* [ ] Step 2: Implement tick()
-* [ ] Step 3: Implement the HLT instruction handler
-* [ ] Step 4: Add the LDI instruction
-* [ ] Step 5: Add the PRN instruction
-* [ ] Step 6: Implement a Multiply and Print the Result
+* [x] Step 2: Implement tick()
+* [x] Step 3: Implement the HLT instruction handler
+* [x] Step 4: Add the LDI instruction
+* [x] Step 5: Add the PRN instruction
+* [x] Step 6: Implement a Multiply and Print the Result
 * [ ] Step 7: Un-hardcode the machine code
 * [ ] Step 8: Beautify your tick() function, if needed
 
--- test.js
+++ test.js
@@ -1,1 +1,45 @@
-const ls8 = require('./project/ls8/ls8');
+const fs = require('fs');
+const path = require('path');
+
+const RAM = require('./project/ls8/ram');
+const CPU = require('./project/ls8/cpu');
+const filename = path.join(__dirname, './project/ls8/print8.ls8');
+const filedata = fs.readFileSync(filename, 'utf8');
+
+// split the file into lines
+const programLines = filedata.trim().split(/[\r\n]+/g);
+
+const loadMemory = (cpu, programCode) => {
+  // const program = [
+  //   // print8.ls8
+  //   '10011001', // LDI R0,8  Store 8 into R0
+  //   '00000000',
+  //   '00001000',
+  //   '01000011', // PRN R0    Print the value in R0
+  //   '00000000',
+  //   '00000001', // HLT       Halt and quit
+  // ];
+
+  // only read the binary part
+  const program = programCode
+    .map(line => parseInt(line, 2).toString(2))
+    .filter(num => !isNaN(num))
+    .map(byte => {
+      const len = byte.length;
+
+      if (len < 8) {
+        for (let i = 0; i < 8 - len; i++) {
+          byte = '0' + byte;
+        }
+      }
+      return byte;
+    });
+
+  program.forEach((code, i) => cpu.poke(i, parseInt(code, 2)));
+};
+
+const ram = new RAM(256);
+const cpu = new CPU(ram);
+
+loadMemory(cpu, programLines);
+cpu.startClock();
